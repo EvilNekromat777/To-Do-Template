d3c5f0329ce98b63454b9e3d47a9cb0f2201df20
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2020-12-05 17:41:42 +0300

    feat: composeItemHTML function

--- scripts/index.js
+++ scripts/index.js
@@ -12,18 +12,20 @@ const listContainerElement = document.querySelector('.todo__list');
 function renderList() {
     let newHTML = '';
 
-    newHTML = TODO_LIST.map(function (item) {
-        return `<li class="todo__item card">
-        <h2 class="card__title">${item.title}</h2>
-        <div class="card__actions">
-          <button type="button" class="button button_duplicate"></button>
-          <button type="button" class="button button_remove"></button>
-        </div>
-      </li>`
-    }).join('');
+    newHTML = TODO_LIST.map(composeItemHTML).join('');
 
-    console.log(newHTML)
     listContainerElement.insertAdjacentHTML('afterbegin', newHTML);
 }
+
+function composeItemHTML(item){
+    return `<li class="todo__item card">
+                <h2 class="card__title">${item.title}</h2>
+                <div class="card__actions">
+                    <button type="button" class="button button_duplicate"></button>
+                    <button type="button" class="button button_remove"></button>
+                </div>
+            </li>`
+}
+
 renderList();
 
